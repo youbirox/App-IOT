2ac9470ea3a06a0013802fd810c54e536eedacd5
Ayoub Moutii <youbirox1994@gmail.com>
2026-04-30 11:30:59 +0200

    Create Vue for DeviceCard and LogList

---
+++ src/components/DeviceCard.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import type { Device } from '../types/device'
+
+defineProps<{
+  device: Device
+}>()
+
+defineEmits<{
+  (e: 'toggle', id: number): void
+}>()
+</script>
+
+<template>
+  <div class="device">
+    <div>
+      <strong>{{ device.name }}</strong>
+      <span :class="device.status ? 'on' : 'off'">
+        {{ device.status ? 'ON' : 'OFF' }}
+      </span>
+    </div>
+
+    <button @click="$emit('toggle', device.id)">Toggle</button>
+  </div>
+</template>
---
+++ src/components/LogList.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import type { Log } from '../types/device'
+
+defineProps<{
+  logs: Log[]
+}>()
+</script>
+
+<template>
+  <div>
+    <div v-for="(log, i) in logs" :key="i" class="log">
+      [{{ log.time }}] {{ log.device }} → {{ log.action }}
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.log {
+  font-size: 14px;
+  padding: 5px 0;
+  border-bottom: 1px solid #eee;
+}
+</style>
